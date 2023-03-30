676b07e3c435ff6f84c7cf75f075f7628d0ceb0b
AHMED KADMIRI <A.KADMIRI@wafaimmo.local>
2023-03-30 09:48:26 +0000

    merge ahmed & amine

---
+++ src/app/modules/auth/sign-in-popup/sign-in-popup.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FuseCardModule } from '@fuse/components/card';
+import { FuseAlertModule } from '@fuse/components/alert';
+import { SharedModule } from 'app/shared/shared.module';
+import { AuthSignInPopupComponent } from './sign-in-popup.component';
+import { AuthSignUpPopupModule } from '../sign-up-popup/sign-up-popup.module';
+
+@NgModule({
+    declarations: [
+        AuthSignInPopupComponent
+    ],
+    imports     : [
+        RouterModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatProgressSpinnerModule,
+        FuseCardModule,
+        FuseAlertModule,
+        SharedModule,
+        AuthSignUpPopupModule,
+    ]
+})
+export class AuthSignInPopupModule
+{
+}
