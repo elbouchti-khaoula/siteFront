ccb7fe418703ffc90d0370ab466790d80decb8b6
KHAOULA ELBOUCHTI <K.ELBOUCHTI@wafaimmo.local>
2023-05-24 16:37:55 +0000

    Updated Ajouter une condition sue les 2 premier nomvre du num de tele

--- src/app/modules/admin/simulation-detaillee/format-tele.directive.ts
+++ src/app/modules/admin/simulation-detaillee/format-tele.directive.ts
@@ -10,8 +10,7 @@ export class FormatTelephoneDirective implements OnInit {
     this.formatTelephone();
   }
 
-  //@HostListener('input')
-  @HostListener('change')
+  @HostListener('input')
   onInputChange() {
     this.formatTelephone();
   }
@@ -20,9 +19,16 @@ export class FormatTelephoneDirective implements OnInit {
     const inputValue = this.elementRef.nativeElement.value;
     const numericValue = inputValue.replace(/\D/g, '');
     const startsWith06or07or05 = /^(05|06|07)/.test(numericValue.substr(0, 2));
-    const phoneNumber = startsWith06or07or05 ? numericValue : '';
-    const formattedPhoneNumber = phoneNumber.slice(0, 10);
-    const formattedValue = formattedPhoneNumber.replace(/(\d{2})(?=\d)/g, '$1-');
+    let formattedValue = '';
+
+    if (numericValue.length >= 2) {
+      const phoneNumber = startsWith06or07or05 ? numericValue : '';
+      const formattedPhoneNumber = phoneNumber.slice(0, 10);
+      formattedValue = formattedPhoneNumber.replace(/(\d{2})(?=\d)/g, '$1-');
+    } else {
+      formattedValue = numericValue;
+    }
+
     this.elementRef.nativeElement.value = formattedValue;
   }
 }
