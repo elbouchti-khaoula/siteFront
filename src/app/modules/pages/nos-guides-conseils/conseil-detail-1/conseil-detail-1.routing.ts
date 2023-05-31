34018d2918fcd06c9e2fb06a21e3ae4dfe22a0a2
ZINEDDINE ISMAILI <Z.ISMAILI@wafaimmo.local>
2023-05-31 17:08:38 +0100

    Details conseils + espace multimedia + key Google map

---
+++ src/app/modules/pages/nos-guides-conseils/conseil-detail-1/conseil-detail-1.routing.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { Detail1Component } from './conseil-detail-1.component';
+
+export const nosGuidesConseilsRoutes: Route[] = [
+    {
+        path     : '',
+        component: Detail1Component
+    }
+];
---
+++ src/app/modules/pages/nos-guides-conseils/conseil-detail-4/conseil-detail-4.routing.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { Detail4Component } from './conseil-detail-4.component';
+
+export const nosGuidesConseilsRoutes: Route[] = [
+    {
+        path     : '',
+        component: Detail4Component
+    }
+];
