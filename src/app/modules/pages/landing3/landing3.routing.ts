7c69715c5c8780400865679b142e31793eec22b8
Fatima-Zahra FAHMI <f.fahmi@wafaimmobilier.co.ma>
2023-06-14 11:56:39 +0000

    3 variantes landing, deplacer contactez-nous

---
+++ src/app/modules/pages/landing3/landing3.routing.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { Landing3Component } from './landing3.component';
+
+export const landing3Routes: Route[] = [
+    {
+        path     : '',
+        component: Landing3Component
+    }
+];
