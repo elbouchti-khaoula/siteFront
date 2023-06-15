e63af416f8b1528072bba4966e4ebb59a5ac983c
K.ELBOUCHTI <K.ELBOUCHTI@wafaimmobilier.co.ma>
2023-06-15 15:38:07 +0000

    update page rse

--- src/app/modules/pages/notre-politique-rse/notre-politique-rse.component.ts
+++ src/app/modules/pages/notre-politique-rse/notre-politique-rse.component.ts
@@ -1,12 +1,15 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation, HostListener } from '@angular/core';
+
 import { trigger, state, style, animate, transition } from '@angular/animations';
 import { fuseAnimations } from '@fuse/animations';
 import { ReferentielService } from 'app/core/referentiel/referentiel.service';
 import { DocumentInstitutionnel } from 'app/core/referentiel/referentiel.types';
 import { Subject, takeUntil } from 'rxjs';
 import { saveAs } from "file-saver";
-import { HttpClient} from '@angular/common/http';
-
+import { HttpClient } from '@angular/common/http';
+import { SwiperOptions } from 'swiper';
+import SwiperCore, { Autoplay, EffectCoverflow, Pagination, Navigation } from "swiper";
+SwiperCore.use([Autoplay, EffectCoverflow, Pagination, Navigation]);
 @Component({
     selector: 'notre-politique-rse',
     templateUrl: './notre-politique-rse.component.html',
@@ -18,7 +21,12 @@ export class NotrePolitiqueRseComponent implements OnInit, OnDestroy {
     isScreenSmall: boolean;
     private _unsubscribeAll: Subject<any> = new Subject<any>();
     chartes: DocumentInstitutionnel[];
+    isMobile: boolean;
 
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+      this.isMobile = window.innerWidth < 768;
+    }
     /**
     * Constructor
     */
@@ -27,8 +35,55 @@ export class NotrePolitiqueRseComponent implements OnInit, OnDestroy {
         private _changeDetectorRef: ChangeDetectorRef,
         private _referentielService: ReferentielService) {
     }
+    
+    pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + '<i></i>' + '<b></b>' + "</span>";
+        },
+    };
+    public slides = [
+        {
+            image: 'assets/images/pages/rse/rse-niv1/Affiche1.png',
+            altText: 'Affiche 1'
+        },
+        {
+            image: 'assets/images/pages/rse/rse-niv1/Affiche2.png',
+            altText: 'Affiche 2'
+        },
+        {
+            image: 'assets/images/pages/rse/rse-niv1/Affiche3.png',
+            altText: 'Affiche 3'
+        },
+        {
+            image: 'assets/images/pages/rse/rse-niv1/Affiche1.png',
+            altText: 'Affiche 4'
+        },
+        {
+            image: 'assets/images/pages/rse/rse-niv1/Affiche2.png',
+            altText: 'Affiche 5'
+        },
+        {
+            image: 'assets/images/pages/rse/rse-niv1/Affiche3.png',
+            altText: 'Affiche 6'
+        },
+        {
+            image: 'assets/images/pages/rse/rse-niv1/Affiche1.png',
+            altText: 'Affiche 7'
+        },
+        {
+            image: 'assets/images/pages/rse/rse-niv1/Affiche2.png',
+            altText: 'Affiche 8'
+        },
+        {
+            image: 'assets/images/pages/rse/rse-niv1/Affiche3.png',
+            altText: 'Affiche 9'
+        },
+
+    ];
 
     ngOnInit(): void {
+        this.isMobile = window.innerWidth < 768; // Définit isMobile en fonction de la largeur de l'écran au chargement de la page
         this._referentielService.documents$
             .pipe(takeUntil(this._unsubscribeAll))
             .subscribe((response: DocumentInstitutionnel[]) => {
@@ -49,12 +104,22 @@ export class NotrePolitiqueRseComponent implements OnInit, OnDestroy {
         this._unsubscribeAll.complete();
     }
 
-  
+
     telecharger() {
         this._httpClient.get('/assets/file/Charte RSE. VF.pdf', { responseType: 'blob' })
             .subscribe((blob: any) => {
                 saveAs(blob, "charte.pdf");
-       });
+            });
     }
 
+    /**
+     * Track by function for ngFor loops
+     *
+     * @param index
+     * @param item
+     */
+    trackByFn(index: number, item: any): any
+    {
+        return item.id || index;
+    }
 }
