d718d14b4a2dbcb31e62b825934506afacae0316
Fatima-Zahra FAHMI <f.fahmi@wafaimmobilier.co.ma>
2023-06-28 12:55:49 +0000

    header instit,fontsize,msg alerte,autre correction

--- src/app/modules/common/page-header/page-header.component.ts
+++ src/app/modules/common/page-header/page-header.component.ts
@@ -7,9 +7,12 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
 })
 export class PageHeaderComponent {
 
+    @Input() hideSmallScreen: boolean = false;
     @Input() headerStyle: string = "";
     @Input() headerLibelle: string;
     @Input() subHeaderLibelle: string;
+    @Input() hideSubHeaderSmallScreen: boolean = false;
+    @Input() textColor: string = "text-default";
 
     /**
      * Constructor
